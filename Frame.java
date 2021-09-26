37b439124b56b09c106b7df6bbfd44d7cba0b2be
Lasyanga <herminigildojrquiano@gmail.com>
2021-09-26 20:57:04 +0800

    first commit

---
+++ Frame.java
@@ -0,0 +1,16 @@
+package Todo;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*; 
+import javax.swing.border.Border;
+
+public class Frame extends JFrame{
+	public Frame(){
+
+	}
+
+	private void initialized(){
+
+	}
+
+}
